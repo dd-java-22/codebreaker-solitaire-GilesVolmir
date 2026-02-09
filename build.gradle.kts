d6a53babdde2795ac899ef55a278e7ace09058e6
Skylar <sscott28@cnm.edu>
2026-02-09 11:29:10 -0700

    Get up to Date with Nick's version

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,11 +14,12 @@
  *  limitations under the License.
  */
 import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 
 plugins {
     java
     jacoco
-    id("org.openapi.generator") version "7.2.0"
+    alias(libs.plugins.openapi)
 }
 
 val javaVersion = libs.versions.java.get()
@@ -29,47 +30,99 @@ java {
     }
 }
 
+sourceSets {
+    main {
+        java {
+            srcDir(layout.buildDirectory.dir("generated/openapi/src/main/java"))
+        }
+    }
+}
+
 dependencies {
+
+    implementation(libs.gson)
     implementation(libs.retrofit.core)
     implementation(libs.retrofit.converter.gson)
     implementation(libs.okhttp)
     implementation(libs.logging.interceptor)
-    implementation(libs.gson)
-    implementation("javax.annotation:javax.annotation-api:1.3.2")
-    implementation("com.google.code.findbugs:jsr305:3.0.2")
+    implementation(libs.jakarta.annotation)
+    implementation(libs.jakarta.validation)
+
     testImplementation(libs.junit.aggregator)
     testRuntimeOnly(libs.junit.engine)
     testRuntimeOnly(libs.junit.platform)
-
 }
 
-openApiGenerate {
-    inputSpec.set("$projectDir/openapi.yaml")
-    generatorName.set("java")
-    apiPackage.set("edu.cnm.deepdive.codebreaker.service")
-    modelPackage.set("edu.cnm.deepdive.codebreaker.model")
-    invokerPackage.set("edu.cnm.deepdive.codebreaker")
-//    outputDir.set("$projectDir/.")
+val openApiGenerator = "java"
+val openApiOutDir = layout.buildDirectory.dir("generated/openapi").get().asFile.toString()
+val openApiSpec = "$projectDir/api/codebreaker.yaml"
+val apiPkg = "${properties.get("basePackage")}.service"
+val modelPkg = "${properties.get("basePackage")}.model"
+val openApiCommonOptions = mapOf(
+    "library" to "retrofit2",
+    "serializationLibrary" to "gson",
+    "dateLibrary" to "java8",
+    "useJakartaEe" to "true",
+    "openApiNullable" to "false",
+    "useBeanValidation" to "false"
+)
+val openApiGlobalProperties = mapOf(
+    "models" to "false",
+    "apis" to "false",
+    "supportingFiles" to "false",
+    "apiDocs" to "false",
+    "apiTests" to "false",
+    "modelDocs" to "false",
+    "modelTests" to "false"
+)
 
-    configOptions.set(
-        mapOf(
-            "library" to "retrofit2",
-            "useRecords" to "true",
-            "generateBuilders" to "true",
-            "dateLibrary" to "java8",
-            "openApiNullable" to "false",
-            "generateModelTests" to "true",
-            "generateApiTests" to "true"
-        )
+tasks.register<GenerateTask>("openApiGenerateModels") {
+
+    generatorName = openApiGenerator
+    inputSpec = openApiSpec
+    outputDir = openApiOutDir
+    apiPackage = apiPkg
+    modelPackage = modelPkg
+    ignoreFileOverride = "$projectDir/api/openapi-generator-ignore"
+
+    configOptions = openApiCommonOptions + mapOf(
+        "generateBuilders" to "true",
+        "useRecords" to "true"
     )
 
-    globalProperties.set(mapOf(
+    globalProperties = openApiGlobalProperties + mapOf(
         "models" to "",
-        "apis" to ""
-    ))
+        "modelDocs" to "true"
+    )
+}
+
+tasks.register<GenerateTask>("openApiGenerateApis") {
+
+    generatorName = openApiGenerator
+    inputSpec = openApiSpec
+    outputDir = openApiOutDir
+    apiPackage = apiPkg
+    modelPackage = modelPkg
+    ignoreFileOverride = "$projectDir/api/openapi-generator-ignore"
+
+    configOptions = openApiCommonOptions + mapOf(
+        "useTags" to "true"
+    )
+
+    globalProperties = openApiGlobalProperties + mapOf(
+        "apis" to "",
+        "apiDocs" to "true",
+        "supportingFiles" to "CollectionFormats.java,StringUtil.java"
+    )
+}
+
+tasks.openApiGenerate {
+    enabled = false
 }
 
 tasks.withType<JavaCompile> {
+    dependsOn(tasks.named("openApiGenerateModels"))
+    dependsOn(tasks.named("openApiGenerateApis"))
     options.release = javaVersion.toInt()
 }
 
@@ -89,13 +142,4 @@ tasks.test {
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
-}
-
-sourceSets {
-    main {
-        java {
-            // This tells Gradle: "Look in both places for source code"
-            srcDirs("${project.buildDir}/generate-resources/main/src/main")
-        }
-    }
 }
--- src/main/java/edu/cnm/deepdive/codebreaker/Main.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/Main.java
@@ -1,12 +1,12 @@
 package edu.cnm.deepdive.codebreaker;
 
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 import edu.cnm.deepdive.codebreaker.model.Game;
+import edu.cnm.deepdive.codebreaker.service.CodebreakerApi;
 import java.io.IOException;
 import java.time.OffsetDateTime;
 import okhttp3.Interceptor;
@@ -18,11 +18,9 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.http.HTTP;
-import edu.cnm.deepdive.codebreaker.model;
-
 
 public class Main {
+
   static void main() {
     Gson gson = new GsonBuilder()
         .registerTypeAdapter(OffsetDateTime.class, new TypeAdapter<OffsetDateTime>() {
@@ -30,7 +28,7 @@ public class Main {
           @Override
           public void write(JsonWriter jsonWriter, OffsetDateTime offsetDateTime)
               throws IOException {
-            jsonWriter.jsonValue(offsetDateTime != null?offsetDateTime.toString():null);
+            jsonWriter.jsonValue(offsetDateTime != null ? offsetDateTime.toString() : null);
           }
 
           @Override
@@ -43,31 +41,32 @@ public class Main {
     Interceptor interceptor = new HttpLoggingInterceptor()
         .setLevel(Level.BODY);
 
-    okhttp3.OkHttpClient client = new OkHttpClient.Builder()
+    OkHttpClient client = new OkHttpClient.Builder()
         .addInterceptor(interceptor)
         .build();
 
-    edu.cnm.deepdive.codebreaker.service.DefaultApi api = new Retrofit.Builder()
+    CodebreakerApi api = new Retrofit.Builder()
         .baseUrl("https://ddc-java.services/codebreaker-solitaire/")
         .addConverterFactory(GsonConverterFactory.create(gson))
         .client(client)
         .build()
-        .create(edu.cnm.deepdive.codebreaker.service.DefaultApi.class);
+        .create(CodebreakerApi.class);
 
-    edu.cnm.deepdive.codebreaker.model.Game game = new edu.cnm.deepdive.codebreaker.model.Game()
+    Game game = new Game()
         .pool("ABCDE")
         .length(2);
 
     api.startGame(game).enqueue(new Callback<>() {
       @Override
       public void onResponse(Call<Game> call, Response<Game> response) {
-        System.out.println("Created this game\n" + response.body());
+        System.out.println("Created a game:" + response.body());
       }
 
       @Override
-      public void onFailure(Call<Game> call, Throwable throwable) {
+      public void onFailure(Call<Game> call, Throwable t) {
         System.out.println("Failure");
       }
     });
+
   }
 }
