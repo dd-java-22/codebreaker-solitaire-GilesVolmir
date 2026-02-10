376549e386698eef78393ed9160ba596edefb51f
Skylar <sscott28@cnm.edu>
2026-02-10 15:59:37 -0700

    Basic gameplay complete for single game. No Validation.

--- src/main/java/edu/cnm/deepdive/codebreaker/Main.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/Main.java
@@ -3,16 +3,53 @@ package edu.cnm.deepdive.codebreaker;
 import edu.cnm.deepdive.codebreaker.model.Game;
 import edu.cnm.deepdive.codebreaker.service.CodebreakerService;
 import edu.cnm.deepdive.codebreaker.viewmodel.GameViewModel;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.LinkedBlockingQueue;
+import okio.Buffer;
 
 public class Main {
 
-  static void main() {
+  private final BlockingQueue<Game> updateQueue = new LinkedBlockingQueue<>();
+  private boolean solved;
 
-    GameViewModel viewModel = new GameViewModel();
-    viewModel.registerGameObserver(System.out::println);
+  void main() {
+
+    GameViewModel viewModel = GameViewModel.getInstance();
+    viewModel.registerGameObserver(updateQueue::add);
+    viewModel.registerSolvedObserver((solved) -> this.solved = Boolean.TRUE.equals(solved));
     viewModel.startGame("ABCDE", 2);
-
     System.out.println("Game start requested!");
+
+    BufferedReader reader = new BufferedReader(new InputStreamReader((System.in)));
+
+    while (!solved) {
+      try {
+        Game game = updateQueue.take();
+        System.out.println(game);
+        if (solved) {
+          System.out.println("You solved it!");
+        } else {
+          // TODO: 2/10/2026 get next guess from user.
+          String rawInput;
+          while ((rawInput = reader.readLine()) != null) {
+            String trimmedInput = rawInput.strip();
+            if (!trimmedInput.isEmpty()) {
+              viewModel.submitGuess(trimmedInput);
+              break;
+            }
+          }
+          // TODO: 2/10/2026 submit new guess.
+
+        }
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
   }
 }
