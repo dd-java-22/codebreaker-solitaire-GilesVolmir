cbff10362630e30d85b2b53d767958e5242bb8b9
Skylar <sscott28@cnm.edu>
2026-02-12 17:08:40 -0700

    GUI work.

---
+++ src/main/java/edu/cnm/deepdive/codebreaker/controller/MainController.java
@@ -0,0 +1,23 @@
+package edu.cnm.deepdive.codebreaker.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class MainController {
+
+  @FXML
+  private Text gameState;
+  @FXML
+  private TextField guessInput;
+  @FXML
+  private Button send;
+
+  @FXML
+  private void submitGuess() {
+
+  }
+}
--- src/main/java/edu/cnm/deepdive/codebreaker/viewmodel/GameViewModel.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/viewmodel/GameViewModel.java
@@ -6,6 +6,7 @@ import edu.cnm.deepdive.codebreaker.service.CodebreakerService;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Consumer;
+import javafx.application.Platform;
 
 @SuppressWarnings({"UnusedReturnValue", "CallToPrintStackTrace", "unused"})
 public class GameViewModel {
@@ -29,6 +30,13 @@ public class GameViewModel {
     solvedObservers = new LinkedList<>();
   }
 
+  /**
+   * REturns a reference to an instance of the class. This class follows the Singleton design
+   * pattern; that is, repeated (or concurrent) calls to this method will all return the same
+   * reference.
+   *
+   * @return
+   */
   public static GameViewModel getInstance() {
     return Holder.INSTANCE;
   }
@@ -124,29 +132,29 @@ public class GameViewModel {
 
   private Game setGame(Game game) {
     this.game = game;
-    gameObservers
-        .forEach((consumer) -> consumer.accept(game));
+    Platform.runLater(() -> gameObservers
+        .forEach((consumer) -> consumer.accept(game)));
     return game;
   }
 
   private Guess setGuess(Guess guess) {
     this.guess = guess;
-    guessObservers
-        .forEach((consumer) -> consumer.accept(guess));
+    Platform.runLater(() -> guessObservers
+        .forEach((consumer) -> consumer.accept(guess)));
     return guess;
   }
 
   private Boolean setSolved(Boolean solved) {
     this.solved = solved;
-    solvedObservers
-        .forEach((consumer) -> consumer.accept(solved));
+    Platform.runLater(() -> solvedObservers
+        .forEach((consumer) -> consumer.accept(solved)));
     return solved;
   }
 
   private Throwable setError(Throwable error) {
     this.error = error;
-    errorObservers
-        .forEach((consumer) -> consumer.accept(error));
+    Platform.runLater(() -> errorObservers
+        .forEach((consumer) -> consumer.accept(error)));
     return error;
   }
 
