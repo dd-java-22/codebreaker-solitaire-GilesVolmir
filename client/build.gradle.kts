e21a10bd9ab25e5b40d77f4d3da5729dcb21e2fd
Skylar <sscott28@cnm.edu>
2026-02-27 12:05:08 -0700

    Migrated Codebreaker client services from Java to Kotlin and updated build configurations.

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -16,11 +16,15 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
-    java
+    kotlin("jvm").version(libs.versions.kotlin.get())
     jacoco
 }
 
 val javaVersion: String = libs.versions.java.get()
+
+kotlin {
+    jvmToolchain(javaVersion.toInt())
+}
 
 java {
     toolchain {
@@ -28,13 +32,17 @@ java {
     }
 }
 
+
+
 dependencies {
     implementation(project(":api"))
+    implementation(kotlin("stdlib"))
     implementation(libs.gson)
     implementation(libs.retrofit.core)
     implementation(libs.retrofit.converter.gson)
     implementation(libs.okhttp)
     implementation(libs.logging.interceptor)
+    testImplementation(kotlin("test"))
     testImplementation(libs.junit.aggregator)
     testRuntimeOnly(libs.junit.engine)
     testRuntimeOnly(libs.junit.platform)
