4f0d2b769561d5b22a64158557fbde66e344f1ac
Skylar <sscott28@cnm.edu>
2026-03-02 16:42:09 -0700

    commiting forgotten junie changes

---
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/app/controller/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.codebreaker.app.controller;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(edu.cnm.deepdive.codebreaker.app.R.layout.activity_main);
+  }
+
+}
