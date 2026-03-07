d8df25ae1c586565b1252fdb4cf9399c7e13fddf
Skylar <sscott28@cnm.edu>
2026-03-07 11:44:47 -0700

    classwork and junie guidelines updates

--- app/src/main/java/edu/cnm/deepdive/codebreaker/app/controller/GameFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/app/controller/GameFragment.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.ViewModelProvider;
 import dagger.hilt.android.AndroidEntryPoint;
 import edu.cnm.deepdive.codebreaker.app.databinding.FragmentGameBinding;
 import edu.cnm.deepdive.codebreaker.app.util.SymbolMap;
+import edu.cnm.deepdive.codebreaker.app.viewmodel.GameViewModel;
 import jakarta.inject.Inject;
 
 @AndroidEntryPoint
@@ -19,6 +23,7 @@ public class GameFragment extends Fragment {
   SymbolMap symbolMap;
 
   private FragmentGameBinding binding;
+  private GameViewModel gameViewModel;
 
   @Nullable
   @Override
@@ -32,7 +37,56 @@ public class GameFragment extends Fragment {
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
-    // TODO: 3/5/2026 Connect to ViewModels and observe LiveData.
+    gameViewModel = new ViewModelProvider(requireActivity()).get(GameViewModel.class);
+    LifecycleOwner lifecycleOwner = getViewLifecycleOwner();
+    gameViewModel
+        .getGame()
+        .observe(lifecycleOwner, (game) -> {
+          // TODO: 3/6/2026 Handle updates to the game
+          //  1. Clear all children from binding.palette
+          //  2. Add a new child for every symbol in the game.getPool().
+          //    a. Inflate a layout for the symbol.
+          //    b. Set the symbol text (contentDescription and tooltip)
+          //    c. Set the symbol Drawable.
+          //    d. Set the symbol Drawable's tint.
+          //    e. Add the symbol widget to the  binding.palette children.
+          binding.palette.removeAllViews();
+          game
+              .getPool()
+              .codePoints()
+              .mapToObj((codepoint) -> {
+                // TODO: 3/6/2026 Inflate a layout and return the inflated widget.  and set cp tag.
+                return (ImageButton) null;
+              })
+              .map((symbolWidget) -> {
+                // TODO: 3/6/2026 Set the symbol text.
+                return symbolWidget;
+              })
+              .map((symbolWidget) -> {
+                // TODO: 3/6/2026 Set the symbol drawable.
+                return symbolWidget;
+              })
+              .map((symbolWidget) -> {
+                // TODO: 3/6/2026 Set the symbol drawable's tint.
+                return symbolWidget;
+              })
+              .forEach((binding.palette::addView));
+        });
+    gameViewModel
+        .getSolved()
+        .observe(lifecycleOwner, (solved) -> {
+          // TODO: 3/6/2026 Handle changes to the solved state of the game.
+        });
+    gameViewModel
+        .getGuess()
+        .observe(lifecycleOwner, (guess) -> {
+          // TODO: 3/6/2026 Handle updates to the most recent guess.
+        });
+    gameViewModel
+        .getError()
+        .observe(lifecycleOwner, (error) -> {
+          // TODO: 3/6/2026 Handle error.
+        });
   }
 
   @Override
